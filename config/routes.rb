14933e16e406c661e735e6c66b1e7546eae92901
ddgriffin <user@server.com>
2012-07-30 19:02:03 -0500

    3rd commit: getting devise up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 GgOnRails::Application.routes.draw do
+
+  resources :pages
+  root :to => "pages#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
